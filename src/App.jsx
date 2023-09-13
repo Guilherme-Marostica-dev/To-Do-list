bea582f8717b6fc829851efd340543d8821cd713
Guilherme Marostica <guilhermesmarostica@gmail.com>
2023-09-13 19:13:52 -0300

    changes for build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TodoList from './Todolist'
+import TodoList from './TodoList.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
